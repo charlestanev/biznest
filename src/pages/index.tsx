a1bc54d645de0740f7fb9473c0fee07e6f398c34
ELMIR CHARLIE TANEV <51314768+charlestanev@users.noreply.github.com>
2025-02-18 18:32:54 +0200

    set up Next.js project with Tailwind CSS, global styles, and basic API route

---
+++ src/pages/index.tsx
@@ -0,0 +1,20 @@
+import localFont from "next/font/local";
+
+const geistSans = localFont({
+  src: "./fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
+});
+const geistMono = localFont({
+  src: "./fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
+});
+
+export default function Home() {
+  return (
+    <>
+      <h1 className="bg-red-600">test</h1>
+    </>
+  );
+}
